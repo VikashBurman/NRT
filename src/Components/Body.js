103eda3b43a019eeac70ec3bc3bbb69cf5fcb20c
Vikash Burman <vikasburman37@gmail.com>
2024-12-28 00:15:18 +0530

    Fetch Api Error Solved

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,15 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [restList, setRestList] = useState(restaurantList);
+  const [restList, setRestList] = useState([]);
+
+  // if(restList.length === 0 ){
+  //   return <Shimmer/>
+  // }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // console.log(json) 
+    setRestList(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+  };
 
   return (
     <div className="body">
       <button
         onClick={() => {
-          const filteredList = restList.filter((res) => res.data.avgRating > 4.2  );
+          const filteredList = restList.filter(
+            (res) => res.data.avgRating > 4.2
+          );
           setRestList(filteredList);
         }}
       >
@@ -17,7 +37,7 @@ const Body = () => {
       </button>
       <div className="res-container">
         {restList.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurantCard   resData={restaurant} />
         ))}
       </div>
     </div>
--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,27 +1,34 @@
 import { CDN_URL } from "../utils/constant";
 
 const RestaurantCard = (props) => {
-    // console.log(props);
-    const {resData} = props;
-    const{cloudinaryImageId,name,cuisines,avgRating,deliveryTime,costForTwo} = resData?.data;
-    return (
-      <div className="res-card">
-        <img
-          className="res-logo"
-          src={CDN_URL+cloudinaryImageId}
-          alt="Meghana Food"
-        />
-        <div className="res-details">
-          <h3 className="res-title">{name}</h3>
-          <p className="res-cuisine">{cuisines.join(",")}</p>
-          <p className="res-cuisine">${costForTwo/100} For Two</p>
-          <div className="res-info">
-            <span className="res-rating">⭐ {avgRating}</span>
-            <span className="res-delivery-time">⏱ {deliveryTime}</span>
-          </div>
+  // console.log(props);
+  const { resData } = props;
+  // console.log(resData.info.name)
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+  } = resData.info;
+  const {deliveryTime} = resData.info.sla
+  // Extract numeric value from the costForTwo string
+  const numericCost = parseInt(costForTwo.replace(/[^0-9]/g, ""), 10);
+
+  return (
+    <div className="res-card">
+      <img className="res-logo" src={CDN_URL+cloudinaryImageId} alt="Food Picture" />
+      <div className="res-details">
+        <h3 className="res-title">{name}</h3>
+        <p className="res-cuisine">{cuisines.join(",")}</p>
+        <p className="res-cuisine">${numericCost} For Two</p>
+        <div className="res-info">
+          <span className="res-rating">⭐ {avgRating}</span>
+          <span className="res-delivery-time">⏱ {deliveryTime} min</span>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default RestaurantCard;
+export default RestaurantCard;
